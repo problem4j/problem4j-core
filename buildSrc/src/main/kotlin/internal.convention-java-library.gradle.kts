c11afd0628c839c1465da16f9c33cb937f565ee8
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-30 11:51:34 +0100

    Re-enable javadoc warnings

--- buildSrc/src/main/kotlin/internal.convention-java-library.gradle.kts
+++ buildSrc/src/main/kotlin/internal.convention-java-library.gradle.kts
@@ -31,13 +31,6 @@ tasks.withType<Jar>().configureEach {
     }
 }
 
-// Disable doclint to avoid errors and warnings on missing JavaDoc comments.
-tasks.withType<Javadoc>().configureEach {
-    (options as StandardJavadocDocletOptions).apply {
-        addStringOption("Xdoclint:none", "-quiet")
-    }
-}
-
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 
