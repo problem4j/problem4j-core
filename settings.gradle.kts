c3ab16ddfe4b0669568160c9316e3ab941a5f8d2
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-01-06 01:32:47 +0100

    Explain rootProject.name change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
 
-// if rootProject.name matches project.name, it fails to generate output for nmcpZipAggregation task, it's probably
-// because there's only one submodule
+// If rootProject.name matches project.name, it fails to generate output for nmcpZipAggregation task - two modules must
+// not have the same name (with root included). That's the reason for rootProject.name not matching repository name.
 rootProject.name = "problem4j-core-root"
 
 include(":problem4j-core")
