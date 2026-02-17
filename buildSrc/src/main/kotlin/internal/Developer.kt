8a139d047d3ffe52addb3bf349c34966dd9a43e8
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-02-17 10:58:35 +0100

    Update fetching boolean propertes in buildscripts

--- buildSrc/src/main/kotlin/internal/Developer.kt
+++ buildSrc/src/main/kotlin/internal/Developer.kt
@@ -1,7 +1,5 @@
 package internal
 
-import org.gradle.api.Project
-
 /**
  * Represents a single developer entry used in publishing metadata (e.g. POM developers section).
  *
@@ -23,41 +21,3 @@ data class Developer(
     val organization: String?,
     val organizationUrl: String?,
 )
-
-/**
- * Evaluates a structured list of developers defined in `gradle.properties`.
- *
- * Developers are resolved using zero-based indexing and the following property naming convention:
- * ```properties
- * internal.pom.developers.0.id=john.doe
- * internal.pom.developers.0.name=JohnDoe
- * internal.pom.developers.0.url=https://johndoe.me
- *
- * internal.pom.developers.1.id=otherdev
- * internal.pom.developers.1.name=Other Developer
- * ```
- *
- * Evaluation stops at the first missing mandatory `id` property. This allows the list to be
- * extended by simply adding the next index.
- *
- * @return An ordered list of resolved [Developer] definitions.
- * @receiver Gradle [Project] from which properties are read.
- */
-fun Project.findDevelopers(): List<Developer> {
-  val developers = mutableListOf<Developer>()
-  var index = 0
-  while (true) {
-    developers.add(
-        Developer(
-            findProperty("internal.pom.developers.$index.id") as? String ?: break,
-            findProperty("internal.pom.developers.$index.name") as? String,
-            findProperty("internal.pom.developers.$index.url") as? String,
-            findProperty("internal.pom.developers.$index.email") as? String,
-            findProperty("internal.pom.developers.$index.organization") as? String,
-            findProperty("internal.pom.developers.$index.organization-url") as? String,
-        )
-    )
-    index++
-  }
-  return developers
-}
--- buildSrc/src/main/kotlin/internal/License.kt
+++ buildSrc/src/main/kotlin/internal/License.kt
@@ -1,7 +1,5 @@
 package internal
 
-import org.gradle.api.Project
-
 /**
  * Represents a single developer entry used in publishing metadata (e.g. POM developers section).
  *
@@ -17,37 +15,3 @@ data class License(
     val distribution: String?,
     val comments: String?,
 )
-
-/**
- * Evaluates a structured list of licenses defined in `gradle.properties`.
- *
- * Licenses are resolved using zero-based indexing and the following property naming convention:
- * ```properties
- * internal.pom.licenses.0.name=MIT License
- * internal.pom.licenses.0.url=https://opensource.org/license/MIT
- *
- * internal.pom.licenses.1.name=Apache-2.0 License
- * ```
- *
- * Evaluation stops at the first missing mandatory `name` property. This allows the list to be
- * extended by simply adding the next index.
- *
- * @return An ordered list of resolved [License] definitions.
- * @receiver Gradle [Project] from which properties are read.
- */
-fun Project.findLicenses(): List<License> {
-  val developers = mutableListOf<License>()
-  var index = 0
-  while (true) {
-    developers.add(
-        License(
-            findProperty("internal.pom.licenses.$index.name") as? String ?: break,
-            findProperty("internal.pom.licenses.$index.url") as? String,
-            findProperty("internal.pom.licenses.$index.distribution") as? String,
-            findProperty("internal.pom.licenses.$index.comments") as? String,
-        )
-    )
-    index++
-  }
-  return developers
-}
