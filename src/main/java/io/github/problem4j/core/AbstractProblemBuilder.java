9f8764f2ec757b0739e0cb6cdd5119124f938f82
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-03-14 20:42:22 +0100

    Deprecate ProblemStatus enum to mark it internal-only (#34)

--- src/main/java/io/github/problem4j/core/AbstractProblemBuilder.java
+++ src/main/java/io/github/problem4j/core/AbstractProblemBuilder.java
@@ -126,7 +126,12 @@ public abstract class AbstractProblemBuilder implements ProblemBuilder, Serializ
    *
    * @param status the {@link ProblemStatus} representing the HTTP status
    * @return this builder instance for chaining
+   * @deprecated {@link ProblemStatus} will be considered an internal utility of Problem4J Core and
+   *     will be made package-private in a future major release. The decision was made as each HTTP
+   *     framework most likely has its own enum for HTTP status codes, and there's no real value in
+   *     trying to maintain a separate, framework-agnostic enum that mirrors HTTP status codes.
    */
+  @Deprecated
   @Override
   public ProblemBuilder status(@Nullable ProblemStatus status) {
     return status != null ? status(status.getStatus()) : status(0);
